9eb09f3369c8adfee1adac291588a39f117b1909
Melissa Connors <40669268+MelissaConnors@users.noreply.github.com>
2020-12-20 17:20:49 -0500

    Create find foreign keys not trusted.sql

---
+++ queries/all-databases/find foreign keys not trusted.sql
@@ -0,0 +1,22 @@
+--find databases by name, count(*) for sys.foreign_keys.is_not_trusted = true
+DECLARE @sql nvarchar(max);
+SET @sql = N'';
+SELECT @sql = @sql + N'UNION ALL 
+  SELECT DBName = N''' + name + ''' COLLATE Latin1_General_BIN, 
+  FKsNotTrusted =  
+  (
+    SELECT COUNT(*) AS FKsNotTrusted
+      FROM ' + QUOTENAME(name) + '.sys.foreign_keys AS f'
+   + N'
+      WHERE f.is_not_trusted = 1 
+        AND f.is_not_for_replication = 0 
+        AND f.is_disabled = 0
+  )
+  ' FROM sys.databases 
+WHERE database_id > 4 AND state = 0;
+
+SET @sql = N'SELECT DBName, FKsNotTrusted FROM 
+(' + STUFF(@sql, 1, 10, N'') 
+   + N') AS x WHERE FKsNotTrusted > 0;';
+
+EXEC sys.sp_executesql @sql;
